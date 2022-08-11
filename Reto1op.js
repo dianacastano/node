cff0eccb6c27adc11561d6d97d00d99c4aea26e7
dianacastano <109579084+dianacastano@users.noreply.github.com>
2022-08-11 10:48:48 +0200

    Retos_Opcionales

---
+++ Reto1op.js
@@ -0,0 +1,30 @@
+const express = require("express");
+
+const app = express();
+
+app.use(express.json());
+
+
+app.get('/', (request, response) => {
+    let name = request.query.name;
+    let status = (name) ? 200 : 400; 
+    let respuesta = (name) ? { ok: true, message: "Petición aceptada", name: name }
+                            : { ok: false, message: "Petición incorrecta" };    
+    response.status(status).json(respuesta);
+});
+
+
+app.post('/', (request, response) => {
+    let respuesta = { ok: false, message: "Petición incorrecta" };
+    let status = 400;
+
+    let { name, usarname, age } = request.body;
+    let jsonIncompleto = (!name || !usarname || !age);
+    status = (jsonIncompleto) ? 400 : 200;
+    respuesta = (jsonIncompleto) ? { ok: false, message: "Petición incorrecta" }
+                                : { ok: true, message: "Petición aceptada", name, usarname, age };
+    
+    response.status(status).json(respuesta);
+});
+
+app.listen(3000)
